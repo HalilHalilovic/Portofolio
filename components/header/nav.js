579c689cc2e534446b54eeffbd1d0d1929d4f7fd
HalilHalilovic <halil.halilovic.w@gmail.com>
2022-11-03 21:58:23 +0100

    HeroSection creation and nav fix

--- components/header/nav.js
+++ components/header/nav.js
@@ -1,8 +1,16 @@
 import Style from "./nav.module.css"
-
+import Link from "next/link";
 function Navbar() {
   return <div className={Style.MainWrap}>
-    <ul></ul>
+    <ul className={Style.ul}>
+        <li className={Style.Link}>
+            <Link href={'/'}>Home</Link>
+        </li>
+        <li className={Style.Link}>
+            <Link href={'/index'}>Contact</Link>
+        </li>
+
+    </ul>
   </div>;
 }
 export default Navbar;
